93c5aac136d76fb03bdbf54868343f7b5d0e6c69
TheVelopRaCuNi <rcuniraon@naver.com>
2017-01-30 02:16:48 +0900

    remove disabled codes

--- src/com/github/RaCuNi/TheCode/main/Main.java
+++ src/com/github/RaCuNi/TheCode/main/Main.java
@@ -1,22 +1,10 @@
 package com.github.RaCuNi.TheCode.main;
 
-//import java.io.InputStreamReader;
-//import java.io.BufferedReader;
-//import java.io.IOException;
+
 
 public class Main {
-	public static void main(String[] args) /*throws IOException*/{
-		/*
-		InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(isr);
-		String UserInput = br.readLine();
-		String UserInput_ = br.readLine();
-		*/
-		//int a = Integer.parseInt(UserInput);
-		//int b = Integer.parseInt(UserInput_);
+	public static void main(String[] args) {
 		DoSummarize sum = new DoSummarize();
-		//int y = sum.sum(a, b);
-		//System.out.println("a:"+a+" "+"b:"+b+" "+"return:"+y);
 	}
 
 }
