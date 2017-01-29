ba0475596379563e9c3dc7e319ae237dce42b6b8
TheVelopRaCuNi <rcuniraon@naver.com>
2017-01-30 02:11:28 +0900

    disable main method's scripts

--- src/com/github/RaCuNi/TheCode/main/Main.java
+++ src/com/github/RaCuNi/TheCode/main/Main.java
@@ -1,20 +1,22 @@
 package com.github.RaCuNi.TheCode.main;
 
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+//import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) /*throws IOException*/{
+		/*
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		String UserInput = br.readLine();
 		String UserInput_ = br.readLine();
-		int a = Integer.parseInt(UserInput);
-		int b = Integer.parseInt(UserInput_);
+		*/
+		//int a = Integer.parseInt(UserInput);
+		//int b = Integer.parseInt(UserInput_);
 		DoSummarize sum = new DoSummarize();
-		int y = sum.sum(a, b);
-		System.out.println("a:"+a+" "+"b:"+b+" "+"return:"+y);
+		//int y = sum.sum(a, b);
+		//System.out.println("a:"+a+" "+"b:"+b+" "+"return:"+y);
 	}
 
 }
