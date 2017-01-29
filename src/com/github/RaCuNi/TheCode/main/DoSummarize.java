7f82cf427486834d21810a62be1aa664090cec0b
TheVelopRaCuNi <rcuniraon@naver.com>
2017-01-30 01:51:51 +0900

    add Files

---
+++ src/com/github/RaCuNi/TheCode/main/DoSummarize.java
@@ -0,0 +1,24 @@
+package com.github.RaCuNi.TheCode.main;
+
+public class DoSummarize {
+	public int sum(int a){
+		int y = a;
+		return y; 
+	}
+	
+	public int sum(int a, int b){
+		int y = a+b;
+		return y;
+	}
+	
+	public int sum(int a, int b, int c){
+		int y = a+b+c;
+		return y;
+	}
+	
+	public int sum(int a, int b, int c, int d){
+		int y = a+b+c+d;
+		return y;
+	}
+
+}
