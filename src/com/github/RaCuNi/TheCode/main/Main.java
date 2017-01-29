73c1dbafceadd376c5db304a6a4f223e983c6cb4
TheVelopRaCuNi <rcuniraon@naver.com>
2017-01-30 03:04:03 +0900

    add compact codes

--- src/com/github/RaCuNi/TheCode/main/Main.java
+++ src/com/github/RaCuNi/TheCode/main/Main.java
@@ -1,10 +1,15 @@
 package com.github.RaCuNi.TheCode.main;
 
-
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		DoSummarize sum = new DoSummarize();
+		int y = sum.sum(a, b);
+		System.out.println("a:"+a+" "+"b:"+b+" "+"return:"+y);
 	}
 
 }
