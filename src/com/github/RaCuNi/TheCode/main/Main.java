3401b726ae1bc553b3145463e4df1fd4df64a9d3
TheVelopRaCuNi <rcuniraon@naver.com>
2017-01-30 03:18:08 +0900

    add program messages

--- src/com/github/RaCuNi/TheCode/main/Main.java
+++ src/com/github/RaCuNi/TheCode/main/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Let's start Summarize!");
+		System.out.println("Type first 'Integer(Ex>1,2,3,4,5 ...)' and second 'Integer'.");
+		System.out.println("(WARNING) Input MUST be 'INTEGER'.");
+		System.out.println("Then Summarizer will Summarize those.");
+		System.out.println("Do it!");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
